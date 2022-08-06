4c13b978a3d1e08f8697c70b13b09bb51678a5bf
Marius König <marius.koenig@outlook.com>
2022-08-06 22:55:40 +0200

    Target state movement animation

--- Assets/Scripts/StateMachines/Player/PlayerTargetingState.cs
+++ Assets/Scripts/StateMachines/Player/PlayerTargetingState.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class PlayerTargetingState : PlayerBaseState
 {
     private readonly int _targetingBlendTreeHash = Animator.StringToHash("TargetingBlendTree");
+    private readonly int _targetingForwardHash = Animator.StringToHash("TargetingForward");
+    private readonly int _targetingRightHash = Animator.StringToHash("TargetingRight");
     public PlayerTargetingState(PlayerStateMachine stateMachine) : base(stateMachine)
     {
     }
@@ -23,8 +25,33 @@ public class PlayerTargetingState : PlayerBaseState
 
         var movement = CalculateMovement();
         Move(movement * StateMachine.TargetingMovementSpeed, deltaTime);
+
+        UpdateAnimator(deltaTime);
         
         FaceTarget();
+    }
+
+    private void UpdateAnimator(float deltaTime)
+    {
+        if (StateMachine.InputReader.MovementValue.y == 0)
+        {
+            StateMachine.Animator.SetFloat(_targetingForwardHash, 0 ,0.1f, deltaTime);
+        }
+        else
+        {
+            var value = StateMachine.InputReader.MovementValue.y > 0 ? 1f : -1f;
+            StateMachine.Animator.SetFloat(_targetingForwardHash, value ,0.1f, deltaTime);
+        }
+        
+        if (StateMachine.InputReader.MovementValue.x == 0)
+        {
+            StateMachine.Animator.SetFloat(_targetingRightHash, 0 ,0.1f, deltaTime);
+        }
+        else
+        {
+            var value = StateMachine.InputReader.MovementValue.x > 0 ? 1f : -1f;
+            StateMachine.Animator.SetFloat(_targetingRightHash, value ,0.1f, deltaTime);
+        }
     }
 
     public override void Exit()
