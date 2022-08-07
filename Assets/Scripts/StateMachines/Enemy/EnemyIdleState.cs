9e9241116651bfd73dce36741bff159415a94528
Marius König <marius.koenig@outlook.com>
2022-08-07 22:23:41 +0200

    Enemy Idle State Part 2

---
+++ Assets/Scripts/StateMachines/Enemy/EnemyIdleState.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyIdleState : EnemyBaseState
+{
+    private readonly int _locomotionHash = Animator.StringToHash("Locomotion");
+    private readonly int _speedHash = Animator.StringToHash("Speed");
+    private const float CrossFadeDuration = 0.1f;
+    private const float DampTime = 0.1f;
+    public EnemyIdleState(EnemyStateMachine stateMachine) : base(stateMachine)
+    {
+        
+    }
+    public override void Enter()
+    {
+        StateMachine.Animator.CrossFadeInFixedTime(_locomotionHash, CrossFadeDuration);
+    }
+
+    public override void Tick(float deltaTime)
+    {
+        Move(deltaTime);
+        if (IsInChaseRange())
+        {
+            Debug.Log("In Range");
+            return;
+        }
+        StateMachine.Animator.SetFloat(_speedHash, 0f, DampTime, deltaTime);
+    }
+
+    protected void Move(Vector3 motion, float deltaTime)
+    {
+        StateMachine.CharacterController.Move((motion + StateMachine.ForceReceiver.Movement) * deltaTime);
+    }
+    
+    protected void Move(float deltaTime)
+    {
+        Move(Vector3.zero, deltaTime);
+    }
+
+    public override void Exit()
+    {
+    }
+}
