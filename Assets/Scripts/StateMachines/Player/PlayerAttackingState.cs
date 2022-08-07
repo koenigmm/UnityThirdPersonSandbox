8b87417366f6724915798ddd61b091eecff32f92
Marius König <marius.koenig@outlook.com>
2022-08-07 15:36:34 +0200

    Play combo attack animations

--- Assets/Scripts/StateMachines/Player/PlayerAttackingState.cs
+++ Assets/Scripts/StateMachines/Player/PlayerAttackingState.cs
@@ -1,22 +1,79 @@
+using UnityEngine;
+
 public class PlayerAttackingState : PlayerBaseState
 {
     private Attack _attack;
+    private float _previousFrameTime;
 
-    public PlayerAttackingState(PlayerStateMachine stateMachine, int attackID) : base(stateMachine)
+    public PlayerAttackingState(PlayerStateMachine stateMachine, int attackIndex) : base(stateMachine)
     {
-        _attack = stateMachine.Attacks[attackID];
+        _attack = stateMachine.Attacks[attackIndex];
     }
 
     public override void Enter()
     {
-        StateMachine.Animator.CrossFadeInFixedTime(_attack.AnimationName, 0.1f);
+        StateMachine.Animator.CrossFadeInFixedTime(_attack.AnimationName, _attack.TransitionDuration);
     }
 
     public override void Tick(float deltaTime)
     {
+        Move(deltaTime);
+        FaceTarget();
+        
+        var normalizedTime = GetNormalizedTime();
+
+        if (normalizedTime > _previousFrameTime && normalizedTime < 1f)
+        {
+            if (StateMachine.InputReader.IsAttacking)
+            {
+                TryComboAttack(normalizedTime);
+            }
+        }
+        else
+        {
+           
+        }
+
+        _previousFrameTime = normalizedTime;
+    }
+
+    private void TryComboAttack(float normalizedTime)
+    {
+        if (_attack.ComboStateIndex == -1) return;
+        if (normalizedTime < _attack.ComboAttackTime) return;
+        
+        StateMachine.SwitchState
+        (
+            new PlayerAttackingState
+            (
+                StateMachine,
+                _attack.ComboStateIndex
+            )
+        );
     }
 
     public override void Exit()
     {
+    }
+
+    private float GetNormalizedTime()
+    {
+        var currentInfo = StateMachine.Animator.GetCurrentAnimatorStateInfo(0);
+        var nextInfo = StateMachine.Animator.GetNextAnimatorStateInfo(0);
+
+        if (StateMachine.Animator.IsInTransition(0) && nextInfo.IsTag("Attack"))
+        {
+            return nextInfo.normalizedTime;
+        }
+
+        else if (!StateMachine.Animator.IsInTransition(0) && currentInfo.IsTag("Attack"))
+        {
+            return currentInfo.normalizedTime;
+        }
+
+        else
+        {
+            return 0f;
+        }
     }
 }
--- Assets/Scripts/StateMachines/Player/PlayerBaseState.cs
+++ Assets/Scripts/StateMachines/Player/PlayerBaseState.cs
@@ -15,6 +15,11 @@ public abstract class PlayerBaseState : State
     {
         StateMachine.CharacterController.Move((motion + StateMachine.ForceReceiver.Movement) * deltaTime);
     }
+    
+    protected void Move(float deltaTime)
+    {
+        Move(Vector3.zero, deltaTime);
+    }
 
     protected void FaceTarget()
     {
