2d7783fe96b9043e53c923c8de7beb10bc1db8cf
Marius König <marius.koenig@outlook.com>
2022-08-09 16:42:21 +0200

    Revert "Ragdoll with bugs"
    This reverts commit 86d0476cc1f7fd89e79de2b90498e7dc24cf800f.

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     public event Action OnTakeDamage;
-    public event Action OnDie;
     [SerializeField] private float maxHealth = 100f;
 
     private float _health;
@@ -20,9 +19,6 @@ public class Health : MonoBehaviour
 
         _health = Mathf.Max(_health - damage, 0f);
         OnTakeDamage?.Invoke();
-        
-        if (_health <= 0f)
-            OnDie?.Invoke();
         
         print(_health);
     }
