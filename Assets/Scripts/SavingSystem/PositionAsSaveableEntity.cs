381eedfd9922af3f12be767b9c4621b74c646b1c
Marius König <marius.koenig@outlook.com>
2022-09-14 22:38:25 +0200

    PositionAsSaveableEntity.cs conflict with PlayerBaseState.cs Move() solved

--- Assets/Scripts/SavingSystem/PositionAsSaveableEntity.cs
+++ Assets/Scripts/SavingSystem/PositionAsSaveableEntity.cs
@@ -1,7 +1,12 @@
-﻿namespace SavingSystem
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace SavingSystem
 {
     public class PositionAsSaveableEntity : SaveableEntity
     {
+        [SerializeField] private bool isAIControlled;
         
 #if UNITY_EDITOR
         private void Update()
@@ -28,8 +33,30 @@
             {
                 if (position.uuid != uuid) continue;
                 print(transform.name);
-                transform.position = position.savedVector3;
+                
+                if (isAIControlled)
+                    HandleAIMovement(position.savedVector3);
+                else
+                    HandlePlayerMovement(position.savedVector3);
+
             }
         }
+
+        private void HandleAIMovement(Vector3 position)
+        {
+            TryGetComponent(out NavMeshAgent navMeshAgent);
+            if (navMeshAgent == null) return;
+            navMeshAgent.Warp(position);
+        }
+
+        private void HandlePlayerMovement(Vector3 position)
+        {
+            TryGetComponent(out CharacterController characterController);
+            if (characterController == null) return;
+            characterController.enabled = false;
+            transform.position = position;
+            characterController.enabled = true;
+        }
+        
     }
 }
